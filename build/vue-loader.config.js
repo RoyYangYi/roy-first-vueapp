039eb4d87f5cecaa8861e1129479eed11138a3c9
Roy_Yang <1165603008@qq.com>
2019-01-16 17:23:52 +0800

    'eslint-error'

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -2,7 +2,11 @@ module.exports = (isDev) => {
     return {
         preserveWhitepace: true,
         extractCSS: !isDev,
-        cssModules:{},
-        hotReload: false,//根据环境变量生成
+        cssModules:{
+            localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base65:5]',
+            camelCase: true
+        },
+        //hotReload: false,//根据环境变量生成
+
     }
 }
